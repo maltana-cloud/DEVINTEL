61e9d3f4ad144defaa63f270b67a094a00e5bd4d
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 19:40:20 -0400

    feat(security): expose security orchestrator

--- devintel/modules/security/__init__.py
+++ devintel/modules/security/__init__.py
@@ -2,13 +2,16 @@
 
 from .contracts import SecurityEvent, SecurityState, ThreatLevel
 from .containment import ContainmentManager
+from .orchestrator import SecurityAction, SecurityOrchestrator
 from .policy import SecurityPolicy
 from .truth import ClaimAssessment, TruthEngine
 
 __all__ = [
     "ClaimAssessment",
     "ContainmentManager",
+    "SecurityAction",
     "SecurityEvent",
+    "SecurityOrchestrator",
     "SecurityPolicy",
     "SecurityState",
     "ThreatLevel",
