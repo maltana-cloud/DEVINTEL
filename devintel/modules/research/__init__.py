e64929cad66325cdb7c419d5871958fcae65b8ee
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 19:01:59 -0400

    feat(research): expose complete research API

--- devintel/modules/research/__init__.py
+++ devintel/modules/research/__init__.py
@@ -1,13 +1,19 @@
 """Knowledge and research primitives for DEVINTEL."""
 
-from .contracts import ResearchCandidate, ResearchDocument, ResearchObservation
-from .pipeline import ResearchPipeline
+from .contracts import ResearchCandidate, ResearchDocument, ResearchObservation, canonicalize_url, content_digest
+from .limits import ResearchLimits
+from .pipeline import ResearchBatch, ResearchPipeline, SourceProvider
 from .store import InMemoryResearchStore
 
 __all__ = [
     "ResearchCandidate",
     "ResearchDocument",
     "ResearchObservation",
+    "canonicalize_url",
+    "content_digest",
+    "ResearchLimits",
+    "ResearchBatch",
     "ResearchPipeline",
+    "SourceProvider",
     "InMemoryResearchStore",
 ]
