7aa0ccccefc2fcad6e08bd3cf07eb7cd13cb3456
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 19:01:26 -0400

    fix(modules): correct package root exports

--- devintel/modules/__init__.py
+++ devintel/modules/__init__.py
@@ -1,13 +1,1 @@
-"""Knowledge and research primitives for DEVINTEL."""
-
-from .contracts import ResearchCandidate, ResearchDocument, ResearchObservation
-from .pipeline import ResearchPipeline
-from .store import InMemoryResearchStore
-
-__all__ = [
-    "ResearchCandidate",
-    "ResearchDocument",
-    "ResearchObservation",
-    "ResearchPipeline",
-    "InMemoryResearchStore",
-]
+"""Pluggable DEVINTEL capability modules."""
