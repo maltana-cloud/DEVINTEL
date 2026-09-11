97eea59921dc96241b84237b14bfe91f8c8258f8
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 22:21:01 -0400

    feat(specialists): export opportunity intelligence engine

--- devintel/modules/specialists/__init__.py
+++ devintel/modules/specialists/__init__.py
@@ -1,5 +1,11 @@
 """Specialist intelligence engines built behind the plugin boundary."""
 
+from .opportunity import OpportunitySpecialist, OpportunitySpecialistResult
 from .research import ResearchSpecialist, ResearchSpecialistResult
 
-__all__ = ["ResearchSpecialist", "ResearchSpecialistResult"]
+__all__ = [
+    "OpportunitySpecialist",
+    "OpportunitySpecialistResult",
+    "ResearchSpecialist",
+    "ResearchSpecialistResult",
+]
