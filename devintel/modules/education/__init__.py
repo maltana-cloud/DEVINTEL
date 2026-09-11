d0bdcad991e4f2c1520a9551af63fd1ac855d225
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-11 17:15:21 -0400

    feat(education): export curriculum primitives

--- devintel/modules/education/__init__.py
+++ devintel/modules/education/__init__.py
@@ -4,13 +4,14 @@ from .contracts import (
     Assessment, AssessmentResult, Course, EducationMode, LearnerProgress,
     LearningPath, Lesson, MentorshipSession, PracticeTask, Skill, SkillLevel,
 )
+from .curriculum import CurriculumVersion
 from .engine import EducationEngine, EducationPlan
 from .policy import EducationPolicy
 from .providers import EducationProvider
 from .store import EducationStore
 
 __all__ = [
-    "Assessment", "AssessmentResult", "Course", "EducationEngine", "EducationMode",
-    "EducationPlan", "EducationPolicy", "EducationProvider", "EducationStore", "LearnerProgress",
-    "LearningPath", "Lesson", "MentorshipSession", "PracticeTask", "Skill", "SkillLevel",
+    "Assessment", "AssessmentResult", "Course", "CurriculumVersion", "EducationEngine",
+    "EducationMode", "EducationPlan", "EducationPolicy", "EducationProvider", "EducationStore",
+    "LearnerProgress", "LearningPath", "Lesson", "MentorshipSession", "PracticeTask", "Skill", "SkillLevel",
 ]
