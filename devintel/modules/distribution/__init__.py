b57072ac0fc44921ba588f2e3c301a7d09227d30
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 20:20:41 -0400

    feat(distribution): expose complete System 5 public interfaces

--- devintel/modules/distribution/__init__.py
+++ devintel/modules/distribution/__init__.py
@@ -1,23 +1,22 @@
-"""Distribution and community capabilities for DEVINTEL."""
+"""System #5 Distribution & Community capabilities."""
 
+from .community import CommunityParticipation, ParticipationRequest, ParticipationResult
 from .contracts import (
-    DestinationKind,
-    DistributionDestination,
-    DistributionMessage,
-    DistributionResult,
-    PublicationDecision,
-    PublicationPolicy,
+    DestinationKind, DistributionDestination, DistributionMessage, DistributionResult,
+    PublicationDecision, PublicationPolicy, PublicationPolicyResult,
 )
-from .router import DistributionRouter
+from .natural import NaturalPublishingDecision, NaturalPublishingEngine, PublishingCandidate
 from .policy import DistributionPolicyEngine
+from .rate_limit import RateLimiter
+from .router import DistributionRouter
+from .service import DistributionService, PublishRequest
+from .telegram import TelegramAdapter, TelegramUpdate
 
 __all__ = [
-    "DestinationKind",
-    "DistributionDestination",
-    "DistributionMessage",
-    "DistributionResult",
-    "DistributionPolicyEngine",
-    "DistributionRouter",
-    "PublicationDecision",
-    "PublicationPolicy",
+    "CommunityParticipation", "ParticipationRequest", "ParticipationResult",
+    "DestinationKind", "DistributionDestination", "DistributionMessage", "DistributionResult",
+    "PublicationDecision", "PublicationPolicy", "PublicationPolicyResult",
+    "NaturalPublishingDecision", "NaturalPublishingEngine", "PublishingCandidate",
+    "DistributionPolicyEngine", "RateLimiter", "DistributionRouter", "DistributionService", "PublishRequest",
+    "TelegramAdapter", "TelegramUpdate",
 ]
