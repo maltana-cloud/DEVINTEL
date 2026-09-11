8f3785dfcc3510bd55c459121e95fe04f996f094
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 21:59:03 -0400

    feat(specialists): export research specialist

--- devintel/modules/specialists/__init__.py
+++ devintel/modules/specialists/__init__.py
@@ -1,1 +1,5 @@
 """Specialist intelligence engines built behind the plugin boundary."""
+
+from .research import ResearchSpecialist, ResearchSpecialistResult
+
+__all__ = ["ResearchSpecialist", "ResearchSpecialistResult"]
