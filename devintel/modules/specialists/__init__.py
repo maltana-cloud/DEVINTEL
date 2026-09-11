1f98fe4b9557ff1b215cf935940b1fdabba86284
maltana-cloud <olalerejuwon22@gmail.com>
2026-09-10 22:36:49 -0400

    feat(specialists): export community intelligence engine

--- devintel/modules/specialists/__init__.py
+++ devintel/modules/specialists/__init__.py
@@ -1,11 +1,18 @@
 """Specialist intelligence engines built behind the plugin boundary."""
 
+from .community import CommunitySignal, CommunitySpecialist, CommunitySpecialistResult
 from .opportunity import OpportunitySpecialist, OpportunitySpecialistResult
 from .research import ResearchSpecialist, ResearchSpecialistResult
+from .truth import TruthSpecialist, TruthSpecialistResult
 
 __all__ = [
+    "CommunitySignal",
+    "CommunitySpecialist",
+    "CommunitySpecialistResult",
     "OpportunitySpecialist",
     "OpportunitySpecialistResult",
     "ResearchSpecialist",
     "ResearchSpecialistResult",
+    "TruthSpecialist",
+    "TruthSpecialistResult",
 ]
